a9ad546c4bdb322e95e91790067a63e7e7a0a096
Michael Pan <panmpan@gmail.com>
2020-11-07 11:17:05 +0800

    Add Flappy Bird demo

---
+++ Assets/Script/FlappyBird/BirdContoller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdContoller : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpVelocity;
+    [SerializeField]
+    private float startDirection;
+    [SerializeField]
+    private float angularVelocity;
+
+    private Vector3 initialPosition;
+    private new Rigidbody2D rigidbody2D;
+
+    private void Awake() {
+        initialPosition = transform.position;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.simulated = false;
+    }
+
+    private void OnEnable() {
+        transform.position = initialPosition;
+        rigidbody2D.simulated = true;
+        rigidbody2D.velocity = new Vector2(0, jumpVelocity);
+        rigidbody2D.angularVelocity = angularVelocity;
+        transform.rotation = Quaternion.Euler(0, 0, startDirection);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, startDirection);
+            rigidbody2D.velocity = new Vector2(0, jumpVelocity);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        enabled = false;
+        GameControl.ins.GameOver();
+    }
+}
---
+++ Assets/Script/FlappyBird/GameControl.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GameControl : MonoBehaviour
+{
+    static public GameControl ins;
+
+    [SerializeField]
+    private GameObject[] groundSetPrefabs;
+
+    [SerializeField]
+    private Vector3 spawnPosition;
+    [SerializeField]
+    private float distroyX;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float spawnGap;
+    private float spawnGapProgess;
+
+    [SerializeField]
+    private BirdContoller birdContoller;
+
+    [SerializeField]
+    private GameObject startText;
+
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+    private int score;
+
+    private List<GameObject> grounds;
+
+    private void Awake() {
+        ins = this;
+        birdContoller.enabled = false;
+        grounds = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        if (!birdContoller.enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                startText.SetActive(false);
+                birdContoller.enabled = true;
+                score = 0;
+                scoreText.text = "0";
+
+                while (grounds.Count > 0)
+                {
+                    Destroy(grounds[0]);
+                    grounds.RemoveAt(0);
+                }
+
+                SpawnGround();
+            }
+        }
+        else
+        {
+            spawnGapProgess += Time.deltaTime;
+            if (spawnGapProgess >= spawnGap)
+                SpawnGround();
+
+            for (int i = 0; i < grounds.Count; i++)
+            {
+                Vector3 position = grounds[i].transform.position;
+                bool cross = position.x <= birdContoller.transform.position.x;
+
+                position.x += moveSpeed * Time.deltaTime;
+
+                if (!cross && position.x <= birdContoller.transform.position.x)
+                {
+                    score += 1;
+                    scoreText.text = score.ToString();
+                }
+
+                if (position.x <= distroyX)
+                {
+                    Destroy(grounds[i]);
+                    grounds.RemoveAt(i);
+                    i--;
+                    break;
+                }
+
+                grounds[i].transform.position = position;
+            }
+        }
+    }
+
+    void SpawnGround()
+    {
+        spawnGapProgess = 0;
+
+        int index = Random.Range(0, groundSetPrefabs.Length);
+        GameObject newGround = Instantiate(groundSetPrefabs[index]);
+        newGround.transform.position = spawnPosition;
+
+        grounds.Add(newGround);
+    }
+
+    public void GameOver()
+    {
+        startText.SetActive(true);
+    }
+}
